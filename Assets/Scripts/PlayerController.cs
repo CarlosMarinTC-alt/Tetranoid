294129818e48646dab4bec06d9e2b07514255df1
CarlosMarinTC-alt <carlosmarintc@gmail.com>
2026-03-05 19:45:35 -0600

    La barra que se controla, ya no se sale de su lugar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,23 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     public float velocidad = 5.5f;
+    public float min = -6.3f;
+    public float max = 6.3f;
+    Rigidbody2D player_RB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player_RB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float velocidadX = Input.GetAxis("Horizontal")*Time.deltaTime*velocidad;
-
-        Vector3 position = transform.position;
-
-        transform.position = new Vector3(velocidadX + position.x, position.y, position.z);
+        float velocidadX = Input.GetAxis("Horizontal");
+        player_RB.linearVelocity = new Vector2(velocidadX*velocidad,player_RB.linearVelocity.y);
+        player_RB.position = new Vector2(Mathf.Clamp(player_RB.position.x, min, max), player_RB.position.y);
     }
 }
